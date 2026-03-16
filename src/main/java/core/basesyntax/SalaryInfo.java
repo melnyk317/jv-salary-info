20f11ebdd988528705413a8e03ac75c18c7c6f26
darina_melnyk <me7darina2008@gmail.com>
2026-03-16 13:53:34 +0200

    Fix code review comments

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,27 +4,34 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
+    private static final DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern("dd.MM.yyyy");
+    private static final int INDEX_OF_DATE = 0;
+    private static final int INDEX_OF_NAME = 1;
+    private static final int INDEX_OF_HOURS = 2;
+    private static final int INDEX_OF_MONEY = 3;
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         int salary = 0;
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         LocalDate date1 = LocalDate.parse(dateFrom, formatter);
         LocalDate date2 = LocalDate.parse(dateTo, formatter);
 
         StringBuilder result = new StringBuilder();
-        result.append("Report for period ").append(dateFrom).append("  - ").append(dateTo);
+        result.append("Report for period ").append(dateFrom).append(" - ").append(dateTo);
 
         for (int i = 0; i < names.length; i++) {
             salary = 0;
-            for (int n = 0; n < data.length; n++) {
-                String[] parts = data[n].split(" ");
-                LocalDate dateToCheck = LocalDate.parse(parts[0], formatter);
-                if (data[n].contains(names[i]) && !dateToCheck.isBefore(date1)
+            for (int j = 0; j < data.length; j++) {
+                String[] parts = data[j].split(" ");
+                LocalDate dateToCheck = LocalDate.parse(parts[INDEX_OF_DATE], formatter);
+                if (parts[INDEX_OF_NAME].equals(names[i]) && !dateToCheck.isBefore(date1)
                         && !dateToCheck.isAfter(date2)) {
-                    salary += Integer.parseInt(parts[2]) * Integer.parseInt(parts[3]);
+                    salary += Integer.parseInt(parts[INDEX_OF_HOURS])
+                            * Integer.parseInt(parts[INDEX_OF_MONEY]);
                 }
             }
-            result.append("\n").append(names[i]).append(" - ").append(salary);
+            result.append(System.lineSeparator()).append(names[i]).append(" - ").append(salary);
         }
 
         return result.toString();
